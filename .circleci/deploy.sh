e34d3f01a5d5a6d2468c6726fdf5c5c1fcef5839
ishakm <6752872+ishakm@users.noreply.github.com>
2019-02-04 13:04:18 -0800

    fix circleci local build error

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
 git config --global user.email $GH_EMAIL
 git config --global user.name $GH_NAME
+
+git clone $CIRCLE_REPOSITORY_URL
+cd core
 
 yarn deploy
